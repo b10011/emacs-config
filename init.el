3e5fa5848da7e3ede207e9b566e0fa1f98649ede
Niko Järvinen <nbjarvinen@gmail.com>
2018-10-23 10:03:15 +0300

    Some packages added

--- init.el
+++ init.el
@@ -20,9 +20,12 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+;; Always ensure the package exists
+(defvar use-package-always-ensure)
+(setq use-package-always-ensure t)
+
 ;; Set up try -package
-(use-package try
-  :ensure t)
+(use-package try)
 
 ;; Remove startup message
 (setq inhibit-startup-message t)
@@ -35,7 +38,6 @@
 
 ;; Set theme and customize it
 (use-package paganini-theme
-  :ensure t
   :config (progn
 	    (load-theme 'paganini t)
 	    (set-face-background 'mode-line "VioletRed2")
@@ -87,19 +89,14 @@ If the new path's directories does not exist, create them."
 (fset 'yes-or-no-p 'y-or-n-p)
 
 ;; Set neotree icon theme if it's graphic display
-(use-package all-the-icons
-  :ensure t)
-(use-package all-the-icons-dired
-  :ensure t)
-(use-package all-the-icons-gnus
-  :ensure t)
-(use-package all-the-icons-ivy
-  :ensure t)
+(use-package all-the-icons)
+(use-package all-the-icons-dired)
+(use-package all-the-icons-gnus)
+(use-package all-the-icons-ivy)
 (defvar neo-theme (if (display-graphic-p) 'icons 'arrow))
 
 ;; Enable .js extension for rjsx
 (use-package rjsx-mode
-  :ensure t
   :config (add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode)))
 
 ;; Set Firefox as default flymd browser
@@ -108,7 +105,6 @@ If the new path's directories does not exist, create them."
   (let ((browse-url-browser-function 'browse-url-firefox))
     (browse-url url)))
 (use-package flymd
-  :ensure t
   :config (setq flymd-browser-open-function 'my-flymd-browser-function))
 
 ;; Remove selection and overwrite
@@ -122,89 +118,62 @@ If the new path's directories does not exist, create them."
           ("lambda" . 955) ; λ
           )))
 (use-package pretty-mode
-  :ensure t
   :hook ((prettify-symbols-mode . update-prettify-symbols-alist)
 	 (python-mode . prettify-symbols-mode)))
 
 ;; Smoother scrolling (leaves room at the edges in form of "prescroll")
 (use-package smooth-scrolling
-  :ensure t
   :config (smooth-scrolling-mode 1))
 
 ;; Smart parens
 (use-package smartparens
-  :ensure t
   :config (smartparens-global-mode t))
 
 ;; Use ibuffer for C-x C-b
 (defalias 'list-buffers 'ibuffer)
 
 ;; Smart command repetition
-(use-package smartrep
-  :ensure t)
+(use-package smartrep)
 
 ;; More string functions (Mainly used with my yasnippets)
-(use-package string-inflection
-  :ensure t)
+(use-package string-inflection)
 
 ;; Import misc packages
-(use-package yasnippet
-  :ensure t)
-
-(use-package auto-sudoedit
-  :ensure t)
-
-(use-package cinspect
-  :ensure t)
-
-(use-package jedi-direx
-  :ensure t)
-
-(use-package flycheck
-  :ensure t)
-
-(use-package cython-mode
-  :ensure t)
-
-(use-package flycheck-cython
-  :ensure t)
-
-(use-package json-mode
-  :ensure t)
-
+(use-package yasnippet)
+(use-package auto-sudoedit)
+(use-package cinspect)
+(use-package jedi-direx)
+(use-package flycheck)
+(use-package cython-mode)
+(use-package flycheck-cython)
+(use-package json-mode)
 (use-package spaceline
-  :ensure t
   :config (spaceline-spacemacs-theme))
-
 (use-package org-bullets
-  :ensure t
   :hook ((org-mode . org-bullets-mode)))
+(use-package 0xc)
 
-(use-package 0xc
-  :ensure t)
+(defvar org-html-postamble nil)
+(add-hook 'org-mode-hook 'visual-line-mode)
 
 (org-babel-do-load-languages
  'org-babel-load-languages
- '((python . t)))
+ '((python . t)
+   (js .t)))
 
-(use-package htmlize
-  :ensure t)
+(use-package htmlize)
 
 (use-package flycheck
-  :ensure t
   :config (global-flycheck-mode 1)
   :hook (flycheck-mode . flycheck-pycheckers-setup))
 
 (use-package flycheck-pycheckers
-  :ensure t
   :config (setq flycheck-pycheckers-checkers '(pylint)))
 
 (use-package hungry-delete
-  :ensure t
   :config (global-hungry-delete-mode))
 
 (use-package which-key
-  :ensure t
   :config (which-key-mode))
 
 ;; Set custom variables
@@ -215,9 +184,10 @@ If the new path's directories does not exist, create them."
  ;; If there is more than one, they won't work right.
  '(elfeed-feeds (quote ("https://www.archlinux.org/feeds/news/")))
  '(global-highlight-parentheses-mode t)
+ '(org-agenda-files (quote ("~/work/hours.org")))
  '(package-selected-packages
    (quote
-    (tss scp atomic-chrome counsel-projectile projectile elfeed better-shell company-tern highlight-parentheses magit zlc which-key try use-package)))
+    (vagrant tss scp atomic-chrome counsel-projectile projectile elfeed better-shell company-tern highlight-parentheses magit zlc which-key try use-package)))
  '(smartparens-global-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -235,20 +205,17 @@ If the new path's directories does not exist, create them."
 
 ;; Set neotree custom navigation shortcuts
 (use-package neotree
-  :ensure t
   :bind (("C-å" . neotree-previous-line)
          ("C-ä" . neotree-next-line)
          ("C-S-n" . neotree-toggle)))
 
 ;; Set up swiper, more interactive version of isearch
 (use-package swiper
-  :ensure t
   :bind (("C-s" . swiper)
          ("C-r" . swiper)))
 
 ;; Set up counsel for completion
 (use-package counsel
-  :ensure t
   :bind (("C-x C-f" . counsel-find-file)
 	 ("M-x" . counsel-M-x)
 	 ("M-y" . counsel-yank-pop)
@@ -256,12 +223,10 @@ If the new path's directories does not exist, create them."
 
 ;; Emacs key bindings for evil-nerd-commenter
 (use-package evil-nerd-commenter
-  :ensure t
   :bind (("M-;" . evilnc-comment-or-uncomment-lines)))
 
 ;; Set up vimish fold
 (use-package vimish-fold
-  :ensure t
   :bind (("C-<dead-acute>" . vimish-fold-toggle)))
 (require 'vimish-fold)
 (vimish-fold-global-mode 1)
@@ -269,19 +234,16 @@ If the new path's directories does not exist, create them."
 
 ;; Set up expand-region
 (use-package expand-region
-  :ensure t
   :bind (("M-h" . er/expand-region)))
 
 ;; Set up avy
 (use-package avy
-  :ensure t
   :bind (("C-c k" . avy-goto-char)
          ("C-c j" . avy-goto-word-1)
          ("C-c l" . avy-goto-line)))
 
 ;; Set ace-window keys
 (use-package ace-window
-  :ensure t
   :bind (("C-x o" . ace-window))
   :config (setq aw-keys '(?a ?s ?d ?f ?q ?w ?e ?r ?t)))
 
@@ -308,7 +270,6 @@ If the new path's directories does not exist, create them."
 
 ;; Spotify controlling
 (use-package spotify
-  :ensure t
   :bind (("M-å" . spotify-playpause)
          ("M-ä" . spotify-next)
          ("M-ö" . spotify-previous))
@@ -316,18 +277,15 @@ If the new path's directories does not exist, create them."
 
 ;; Reversible C-x 1 (for temporary fullscreen of a buffer)
 (use-package zygospore
-  :ensure t
   :config (global-set-key (kbd "C-x 1") 'zygospore-toggle-delete-other-windows))
 
 ;; Set up multiple-cursors
 (use-package multiple-cursors
-  :ensure t
   :bind (("C-<" . mc/mark-next-like-this)
          ("C-M-<" . mc/skip-to-next-like-this)
          ("C-c r" . mc/mark-all-in-region))
   :config (define-key mc/keymap (kbd "<return>") nil))
 (use-package mc-extras
-  :ensure t
   :bind (("C-c f" . mc/freeze-fake-cursors)
          ("C-c u" . mc/unfreeze-fake-cursors)
          ("C-c c" . mc/remove-cursors-on-blank-lines)))
@@ -344,60 +302,55 @@ If the new path's directories does not exist, create them."
 
 ;; Set up ivy
 (use-package ivy
-  :ensure t
-  :config (setq ivy-use-virtual-buffers t)
+  :config (progn
+	    (setq ivy-use-virtual-buffers t)
+	    (global-set-key (kbd "C-c c") #'ivy-resume))
   :bind (("C-x b" . ivy-switch-buffer)
 	 ("C-c v" . ivy-push-view)
 	 ("C-c V" . ivy-pop-view)))
 
 ;; Set up Magit
-(use-package magit
-  :ensure t)
+(use-package magit)
 
 ;; Set up zlc, zsh like autocompletion
-(use-package zlc
-  :ensure t)
+(use-package zlc)
 
 ;; Set up highlight-parentheses
 (use-package highlight-parentheses
-  :ensure t
   :config (global-highlight-parentheses-mode))
 
 ;; Set up better-shell
 ;; TODO: check if one could use remote's default shell when opening remote shell
 (use-package better-shell
-  :ensure t
   :bind (("C-'" . better-shell-shell)
          ("C-*" . better-shell-remote-open)))
 
 ;; Set up elfeed
-(use-package elfeed
-  :ensure t)
+(use-package elfeed)
 
 ;; Set up projectile and counsel-projectile
 (use-package projectile
-  :ensure t
   :config (progn
             (projectile-mode +1)
             (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
             (defvar projectile-completion-system 'ivy)))
 (use-package counsel-projectile
-  :ensure t
   :config (counsel-projectile-mode +1))
 ;; Fixes .projectile fails to ignore folder -bug
 (setq projectile-indexing-method 'native)
 
 ;; Set up atomic-chrome
 (use-package atomic-chrome
-  :ensure t
   :config (atomic-chrome-start-server))
 
-(use-package scp
-  :ensure t)
+;; Set up scp
+(use-package scp)
+
+;; Set up vagrant
+(use-package vagrant)
 
 ;; Set up company for autocompletion
 (use-package company
-  :ensure t
   :config (progn
             (global-company-mode)
             (setq company-idle-delay 0)
@@ -405,13 +358,11 @@ If the new path's directories does not exist, create them."
 
 ;; Set up company-tern for JavaScript autocompletion
 (use-package company-tern
-  :ensure t
   :config (add-to-list 'company-backends 'company-tern)
   :hook ((rjsx-mode . tern-mode)))
 
 ;; Set up jedi for Python autocompletion
 (use-package jedi
-  :ensure t
   :config (progn
             (jedi:install-server)
             (setq jedi:complete-on-dot t))
